1f4ccb10ff9333e7709cc4bf27d7bba369c31316
Joanna Piper Morgan (jonsey) <jpmorgan98@gmail.com>
2024-03-29 10:21:57 -0700

    wip

--- kobyashi/mcdc/run.py
+++ kobyashi/mcdc/run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import timeit
-
+import time
 
 N_list = np.logspace(5, 7, 3).astype(int)
 runtimes = np.zeros(9)
@@ -10,13 +10,13 @@ runtimes = np.zeros(9)
 for i in range( N_list.size ):
    print("Running {0} particles".format(N_list[i]))
 
-   start = timeit.timeit()
+   start = time.time()
 
    os.system("srun -n 36 python input.py"
         + " --mode=numba --N_particle=%i"%int(N_list[i])
         + " --output=output_%i"%int(N_list[i]))
 
-   end = timeit.timeit()
+   end = time.time()
    print(start-end)
    runtimes[i] = end-start
 
