16c33b9af2a4d5ed1a7e7e76932ee4fe494ddc21
Joanna Piper Morgan (jonsey) <jpmorgan98@gmail.com>
2024-03-29 10:18:47 -0700

    adding files I had moved to a directory

---
+++ kobyashi/mcdc/run.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+import timeit
+
+
+N_list = np.logspace(5, 7, 3).astype(int)
+runtimes = np.zeros(9)
+
+
+for i in range( N_list.size ):
+   print("Running {0} particles".format(N_list[i]))
+
+   start = timeit.timeit()
+
+   os.system("srun -n 36 python input.py"
+        + " --mode=numba --N_particle=%i"%int(N_list[i])
+        + " --output=output_%i"%int(N_list[i]))
+
+   end = timeit.timeit()
+   print(start-end)
+   runtimes[i] = end-start
+
+   print("Runtime: {0}".format( runtimes[i] ))
+   print()
+
+print(runtimes)
+
+np.savez('mcdc_kobyashi_runtimes.npz', N_list = N_list, runtimes=runtimes)
